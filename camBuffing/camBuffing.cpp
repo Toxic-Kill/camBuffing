c88146751aad5dda6d0b836799d761578ccf0868
Kill Toxic <913087364@qq.com>
2020-03-27 15:43:08 +0800

    camBuffing改

--- camBuffing/camBuffing.cpp
+++ camBuffing/camBuffing.cpp
@@ -22,13 +22,16 @@ int main()
 		cv::Mat frame;
 		cv::Mat hsvMat;
 		cv::Mat detectMat;
+		cv::Mat gauMat;
 		cv::Mat bufMat;
-		cap >> frame;
-		cvtColor(frame, hsvMat, COLOR_BGR2HSV);
-		cv::inRange(hsvMat, Scalar(i_minH, i_minS, i_minV), Scalar(i_maxH, i_maxS, i_maxV), detectMat);
-		cvtColor(detectMat, bufMat, COLOR_HSV2BGR);
-		cv::imshow("frame", frame);
-		cv::imshow("buffing", bufMat);
+		cap >> frame;//捕捉当前帧
+		cvtColor(frame, hsvMat, COLOR_BGR2HSV);//转为HSV格式
+		cv::inRange(hsvMat, Scalar(i_minH, i_minS, i_minV), Scalar(i_maxH, i_maxS, i_maxV), detectMat);//捕捉皮肤
+		medianBlur(frame, gauMat, 7);//对当前帧进行中值滤波
+		frame.copyTo(bufMat);//将当前帧深度复制到bufMat中
+		gauMat.copyTo(bufMat, detectMat);//将中值滤波后的皮肤部分遮罩在当前帧上
+		cv::imshow("frame", frame);//显示当前帧
+		cv::imshow("buffing", bufMat);//显示磨皮处理后的当前帧
 		waitKey(30);
 	}
 }
